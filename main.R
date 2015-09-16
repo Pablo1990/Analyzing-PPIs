a7a61394098e4f11eb752e968ce313fda925ece1
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-09-16 16:54:02 +0100

    radius calculated

--- main.R
+++ main.R
@@ -8,8 +8,10 @@ library(igraph)
 vertices <- 4561
 edges <- 7906 #Should verify this number
 
+r <- sqrt((edges*2)/(vertices*(vertices-1)*pi))
 #radius on the paper? They didn't say anything...
-geometric <- grg.game(nodes = vertices, radius = 0.05, torus = F) #Torus: F => square, T => a Torus
+
+geometric <- grg.game(nodes = vertices, radius = r, torus = F) #Torus: F => square, T => a Torus
 
 #http://rgm3.lab.nig.ac.jp/RGM/R_rdfile?f=igraph/man/static.power.law.game.Rd&d=R_CC
 #They use exponent.out = 2.1 - 2.4
@@ -18,6 +20,7 @@ sf21 <- static.power.law.game (no.of.nodes = vertices, no.of.edges = edges, expo
 #BA model (scale-free)
 #They use power = 2.1 - 2.4
 #sf21 <- barabasi.game(n = vertices, power = 2.1, m = vertices/10, directed = F)
+
 
 
 
