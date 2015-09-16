5d7e2278f98d7f014efb6265ca7fb6513b774c66
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-09-16 15:04:18 +0100

    Idk what i'm doing haha

--- main.R
+++ main.R
@@ -6,15 +6,18 @@
 library(igraph)
 
 vertices <- 4561
+edges <- 7906 #Should verify this number
 
-#radius on the paper?
+#radius on the paper? They didn't say anything...
 geometric <- grg.game(nodes = vertices, radius = 0.05, torus = F) #Torus: F => square, T => a Torus
 
 #http://rgm3.lab.nig.ac.jp/RGM/R_rdfile?f=igraph/man/static.power.law.game.Rd&d=R_CC
 #They use exponent.out = 2.1 - 2.4
-#Useless -> not random edges? O.o
-#scalefree21 <- static.power.law.game (no.of.nodes = vertices, exponent.out = 2.1, exponent.in = -1, multiple = T)
+sf21 <- static.power.law.game (no.of.nodes = vertices, no.of.edges = edges, exponent.out = 2.1, exponent.in = -1, multiple = T)
 
 #BA model (scale-free)
 #They use power = 2.1 - 2.4
-barabasi.game(n = vertices, power = 2.1)
+#sf21 <- barabasi.game(n = vertices, power = 2.1, m = vertices/10, directed = F)
+
+
+
