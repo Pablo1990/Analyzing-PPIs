993e13335b436908b7fd2f7f8b9fce7bf939bf49
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-09-17 10:40:06 +0100

    Written the proper network file (sf)

--- main.R
+++ main.R
@@ -6,16 +6,28 @@
 library(igraph)
 
 vertices <- 4561
-edges <- 7906 #Should verify this number
+rEdges <- 7906 #Should verify this number: 7906 (suppose is the real one)
+
+#The tunned one
+edges <- 8015
 
 r <- sqrt((edges*2)/(vertices*(vertices-1)*pi))
 #radius on the paper? They didn't say anything...
 
 geometric <- grg.game(nodes = vertices, radius = r, torus = F) #Torus: F => square, T => a Torus
 
+summary(geometric)
+
 #http://rgm3.lab.nig.ac.jp/RGM/R_rdfile?f=igraph/man/static.power.law.game.Rd&d=R_CC
 #They use exponent.out = 2.1 - 2.4
-sf21 <- static.power.law.game (no.of.nodes = vertices, no.of.edges = edges, exponent.out = 2.1, exponent.in = -1, multiple = T)
+sf21 <- static.power.law.game (no.of.nodes = vertices, no.of.edges = rEdges, exponent.out = 2.1, exponent.in = -1, multiple = T)
+
+eSf21 <- E(sf21) #edges time!
+
+
+fileConn<-file("sfEdges.sf")
+write.graph(sf21, file = "sfEdges.sf", format = "edgelist")
+close(fileConn)
 
 #BA model (scale-free)
 #They use power = 2.1 - 2.4
