ae14e967fdec2f6aadd9695a68f934bfcf90fdab
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-09-17 10:39:46 +0100

    bash script to execute graphletCounter

---
+++ src/graphCounter.sh
@@ -0,0 +1,7 @@
+#Code wrote by Pablo Vicente-Munuera
+
+cd /Users/pablovm1990/Documents/Dropbox/MScBioinformatics/Thesis/Project/software/GraphletCounter-1.2/bin 
+./gc.sh /Users/pablovm1990/Documents/Dropbox/MScBioinformatics/Thesis/Project/data/Y2H-SaccharomycesCerevisiae.txt -all
+
+#
+
