ace739bae4e2d37937575e92db33d523431962e9
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-09-23 13:41:56 +0100

    executing the distance between models

--- main.R
+++ main.R
@@ -36,12 +36,15 @@ generateRandomNetworks(vertices = 4141, edges = 7686, rEdges = 7786, rRRG = rRRG
 
 
 
-# ---------------- Reading results ------------#
+# ---------------- Distance results ------------#
 
 source('~/Documents/Dropbox/MScBioinformatics/Thesis/Project/Analyzing-PPIs/src/showGraphletCountInfo.R', echo=TRUE)
 source('~/Documents/Dropbox/MScBioinformatics/Thesis/Project/Analyzing-PPIs/src/distanceBetweenGraphlets.R', echo=TRUE)
 
 #layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE))
+
+    #-- SCerevisiae
+nameSC <- "SCerevisiae"
 
 resultsGeo2D <- showGraphletCountInformationFromRandom(fileNameStart = "data/results/randomGeo2DNetwork", nameOfOriginal = nameSC)
 original <- read.csv(file = "data/results/Y2H-SaccharomycesCerevisiae.txtGC.txt", sep = "\t")
@@ -49,7 +52,46 @@ resultsOriginal <- showGraphletCountInformation(original)
 
 print(distanceBetweenGraphlets(resultsOriginal, resultsGeo2D))
 
-#-----------------------------------------------
+resultsSf <- showGraphletCountInformationFromRandom(fileNameStart = "data/results/randomSFNetwork", nameOfOriginal = nameSC)
+print(distanceBetweenGraphlets(resultsOriginal, resultsSf)) #Not Yet!
+
+resultsRRG <- showGraphletCountInformationFromRandom(fileNameStart = "data/results/randomRRGNetwork", nameOfOriginal = nameSC)
+print(distanceBetweenGraphlets(resultsOriginal, resultsRRG))
+
+  #-- CElegans
+nameCEle <- "CElegans"
+
+resultsGeo2D <- showGraphletCountInformationFromRandom(fileNameStart = "data/results/randomGeo2DNetwork", nameOfOriginal = nameCEle)
+original <- read.csv(file = "data/results/Y2H-CaenorhabditisElegans.txtGC.txt", sep = "\t")
+resultsOriginal <- showGraphletCountInformation(original)
+
+print(distanceBetweenGraphlets(resultsOriginal, resultsGeo2D))
+
+resultsSf <- showGraphletCountInformationFromRandom(fileNameStart = "data/results/randomSFNetwork", nameOfOriginal = nameCEle)
+print(distanceBetweenGraphlets(resultsOriginal, resultsSf))
+
+resultsRRG <- showGraphletCountInformationFromRandom(fileNameStart = "data/results/randomRRGNetwork", nameOfOriginal = nameCEle)
+print(distanceBetweenGraphlets(resultsOriginal, resultsRRG))
+
+    #--- Drosophila
+nameDroso <- "Drosophila"
+
+resultsGeo2D <- showGraphletCountInformationFromRandom(fileNameStart = "data/results/randomGeo2DNetwork", nameOfOriginal = nameDroso)
+original <- read.csv(file = "data/results/Y2H-DrosophilaMelanogaster.txtGC.txt", sep = "\t")
+resultsOriginal <- showGraphletCountInformation(original)
+
+print(distanceBetweenGraphlets(resultsOriginal, resultsGeo2D))
+
+resultsSf <- showGraphletCountInformationFromRandom(fileNameStart = "data/results/randomSFNetwork", nameOfOriginal = nameDroso)
+print(distanceBetweenGraphlets(resultsOriginal, resultsSf))
+
+resultsRRG <- showGraphletCountInformationFromRandom(fileNameStart = "data/results/randomRRGNetwork", nameOfOriginal = nameDroso)
+print(distanceBetweenGraphlets(resultsOriginal, resultsRRG))
+
+
+
+
+# ---------------- Reading results ------------#
 
 #http://statmethods.net/graphs/line.html
 plot(resultsGeo2D, main = "Saccharomyces", xlab = "Type of graphlet", ylab = "Frequencies", type="l",col="red")
